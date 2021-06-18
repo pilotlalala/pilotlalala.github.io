0e9463a3334e422f263c4a2b2d2ba42c110b158b
pilotlalala <2072281836@qq.com>
2021-06-18 18:24:19 +0800

    Site updated: 2021-06-18 18:24:19

--- js/say.js
+++ js/say.js
@@ -25,16 +25,8 @@ function fetchApiToSay() {
       .then((res) => {
         if (res.ok) {
           res.json().then((data) => {
-            if (CONFIG.say.hitokoto) {
               say(data.hitokoto, data.from_who, data.from);
-            } else {
-              let sentence = data[Math.floor(Math.random() * data.length)];
-              if (sentence.content) {
-                say(sentence.content, sentence.author, sentence.from);
-              } else {
-                say(sentence);
-              }
-            }
+
           });
         } else {
           throw new Error(
