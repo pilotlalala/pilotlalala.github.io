a3100a09383a7d2c106df1df9828612239ccde08
pilotlalala <2072281836@qq.com>
2021-06-18 19:15:50 +0800

    Site updated: 2021-06-18 19:15:49

--- js/say.js
+++ js/say.js
@@ -32,7 +32,7 @@ function fetchApiToSay() {
               if (sentence.content) {
                 say(sentence.content, sentence.author, sentence.from);
               } else {
-                say(sentence,sentence,sentence);
+                say(sentence,data[1],data[2]);
               }
             }
           });
